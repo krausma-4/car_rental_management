4aee783f608052f5012c365245354a7e0900cde8
Franz Gabriel <gabrifr1@b-tu.de>
2020-06-17 17:28:34 +0200

    hmtls satrted

--- public/myscript.js
+++ public/myscript.js
@@ -1,7 +1,20 @@
 // initialize Firestore database
 const db = firebase.firestore();
 
-// list all book records of the collection "books"
+db.collection("cars").add( {
+    licensePlate: 1111,
+    manufacturer: "Audi",
+    model: "A6 Avant",
+    damages:["", "", "", "", "",]
+     })
+    .then(function(carRef) {
+      console.log("Car written");
+    })
+    .catch(function(error) {
+      console.error("Error adding car: ", error);
+});
+
+// list all car records of the collection "cars"
 let ulEl = document.getElementById("myList");
 db.collection("cars").get().then( function (cars) {
   cars.forEach( function (car) {
@@ -10,7 +23,7 @@ db.collection("cars").get().then( function (cars) {
     liEl.innerHTML += c.licensePlate + " | " + c.manufacturer + " | " + c.model;
     ulEl.appendChild( liEl);
   });
-  console.log("All books' listed");
+  console.log("All cars listed");
 }).catch(function( error) {
-  console.error("Error retrieving book records: ", error);
+  console.error("Error retrieving car records: ", error);
 })
