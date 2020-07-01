a7511759e4944b12e9727622e3681cda5767ef70
kostoele <kostoele@b-tu.de>
2020-07-01 23:38:43 +0200

    create and retrieve rental Agreement in manageRA.js

---
+++ public/src/m/RentalAgreement.js
@@ -0,0 +1,159 @@
+class RentalAgreement {
+    constructor({ invoiceId, customer, car, startDate, endDate, price }) {
+        (this._invoiceId = ""),
+        (this._customer = {}),
+        (this._car = {}),
+        (this._startDate = ""),
+        (this._endDate = ""),
+        (this._price = "");
+
+        this.invoiceID = invoiceId;
+        this.customer = customer;
+        this.car = car;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.price = price;
+    }
+    get invoiceID() {
+        return this._invoiceId;
+    }
+
+    set invoiceID(id) {
+        this._invoiceId = id;
+    }
+
+    get customer() {
+        return this._customer;
+    }
+
+    set customer(cust) {
+        this._customer = cust;
+    }
+
+    get car() {
+        return this._car;
+    }
+    set car(c) {
+        this._car = c;
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(s) {
+        this._startDate = s;
+    }
+
+    get endDate() {
+        return this._endDate;
+    }
+
+    set endDate(e) {
+        this._endDate = e;
+    }
+
+    get price() {
+        return this._price;
+    }
+
+    set price(p) {
+        this._price = p;
+    }
+}
+
+RentalAgreement.retrieveAll = async function() {
+    try {
+        let rentRecords = (
+            await db.collection("rentalAgreements").get()
+        ).docs.map((d) => d.data());
+        console.log(`${rentRecords.length} rental agreements records retrieved`);
+        return rentRecords;
+    } catch (error) {
+        console.error(`Error getting rent records: ${error}`);
+    }
+};
+
+RentalAgreement.retrieve = async function(rentID) {
+    try {
+        let rentRecord = (
+            await db.collection("rentalAgreements").doc(rentID).get()
+        ).data();
+        return rentRecord;
+    } catch (error) {
+        console.error(`Error getting rent records: ${error}`);
+    }
+};
+
+RentalAgreement.add = async function(slots) {
+    await db.collection("rentalAgreements").doc(slots.invoiceId).set(slots);
+    console.log(`Rent record ${slots.invoiceId} created.`);
+};
+
+RentalAgreement.update = async function(slots) {
+    if (Object.keys(slots).length > 0) {
+        await db.collection("rentalAgreements").doc(slots.invoiceId).update(slots);
+        console.log(`Rent record ${slots.invoiceId} modified.`);
+    }
+};
+
+RentalAgreement.destroy = async function(rentID) {
+    try {
+        await db.collection("rentalAgreements").doc(rentID).delete();
+        console.log(`Rent record ${rentID} deleted.`);
+    } catch (error) {
+        console.error(`Error deleting rent record ${error}`);
+    }
+};
+
+RentalAgreement.generateTestData = function() {
+    let rentRecords = {};
+    rentRecords["11344"] = {
+        invoiceId: "112233",
+        customer: {
+            customersId: "12",
+            name: "Emin",
+            surname: "Bob",
+            dateOfBirth: Date(12 - 05 - 1995),
+            address: "Erich-Weinert-Strasse 6",
+        },
+        car: { licensePlate: "556677", manufacturer: "Toyota", model: "Corolla" },
+        startDate: Date(12 - 05 - 1994),
+        endDate: Date(12 - 05 - 1995),
+        price: "179,98",
+    };
+    rentRecords["2143245"] = {
+        invoiceId: "123467",
+        customer: {
+            customersId: "12",
+            name: "Martin",
+            surname: "Haris",
+            dateOfBirth: Date(12 - 05 - 1995),
+            address: "Erich-Weinert-Strasse 6",
+        },
+        car: { licensePlate: "445566", manufacturer: "Tesla", model: "Model X" },
+        startDate: Date(12 - 05 - 1995),
+        endDate: Date(20 - 05 - 1995),
+        price: "289",
+    };
+
+    // Save all test Book records to Firestore DB
+    for (let id of Object.keys(rentRecords)) {
+        let rentRecord = rentRecords[id];
+        db.collection("rentalAgreements").doc(id).set(rentRecord);
+    }
+    console.log(`${Object.keys(rentRecords).length} rent agreements saved.`);
+};
+
+RentalAgreement.clearData = function() {
+    if (confirm("Do you really want to delete all rent Agreements?")) {
+        db.collection("rentalAgreements")
+            .get()
+            .then(function(allRents) {
+                // Delete book docs iteratively
+                allRents.forEach(function(rentDocs) {
+                    db.collection("rentalAgreements").doc(rentDocs.id).delete();
+                });
+            });
+    }
+};
--- public/src/v/manageRentalAgreement.js
+++ public/src/v/manageRentalAgreement.js
@@ -1,23 +1,87 @@
 car_rental.v.rentalAgreements.listAllRentAgreements = {
     setupUserInterface: async function() {
+        const tableBodyEl = document.querySelector("table#rents>tbody");
 
+        var rentRecords = await RentalAgreement.retrieveAll();
+
+        for (let rentRec of rentRecords) {
+
+            let row = tableBodyEl.insertRow();
+            row.insertCell(-1).textContent = rentRec.invoiceId;
+            row.insertCell(-1).textContent = rentRec.customer.name;
+            row.insertCell(-1).textContent = rentRec.car.model;
+            row.insertCell(-1).textContent = rentRec.startDate;
+            row.insertCell(-1).textContent = rentRec.endDate;
+            row.insertCell(-1).textContent = rentRec.price;
+        }
     },
 };
 
 car_rental.v.rentalAgreements.createRentAgreement = {
     setupUserInterface: async function() {
+        const formEl = document.forms["Rent-C"],
+            saveButton = document.forms["Rent-C"].commit,
+            selectCustEl = formEl.customer,
+            selectCarEl = formEl.car;
 
+        var allCustomers = await Customer.retrieveAll();
+        var allCars = await Car.retrieveAll();
+        let customer, car;
+
+        for (let custRec of allCustomers) {
+            let optionEl = document.createElement("option");
+            optionEl.text = custRec.name;
+            optionEl.value = custRec.customersId;
+            selectCustEl.add(optionEl, null);
+        }
+
+        for (let carRec of allCars) {
+            let optionEl = document.createElement("option");
+            optionEl.text = carRec.model;
+            optionEl.value = carRec.licensePlate;
+            selectCarEl.add(optionEl, null);
+        }
+
+        selectCustEl.addEventListener("change", function() {
+            for (let custRec of allCustomers) {
+                if (custRec.customersId === selectCustEl.value) {
+                    customer = custRec;
+                }
+            }
+        });
+
+        selectCarEl.addEventListener("change", function() {
+            for (let carRec of allCars) {
+                if (carRec.licensePlate === selectCustEl.value) {
+                    car = carRec;
+                }
+            }
+        });
+
+        // set an event handler for the submit/save button
+        saveButton.addEventListener("click", async function() {
+
+            //await db.collection("cars").doc(selectCarEl.value).get().data()
+            const slots = {
+                invoiceId: formEl.invoiceId.value,
+                customer: customer,
+                car: {},
+                startDate: formEl.start.value,
+                endDate: formEl.end.value,
+                price: formEl.price.value,
+            };
+            alert("I am creatting now");
+            await RentalAgreement.add(slots);
+            formEl.reset();
+        });
     },
+
 };
 
 car_rental.v.rentalAgreements.updateRentAgreement = {
-    setupUserInterface: async function() {
-
-    },
+    setupUserInterface: async function() {},
 };
 
 car_rental.v.rentalAgreements.deleteRentAgreement = {
-    setupUserInterface: async function() {
-
-    },
+    setupUserInterface: async function() {},
 };
