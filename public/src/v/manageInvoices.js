da2ec87aac6d561ec34790463c47c536b5d1789c
kostoele <kostoele@b-tu.de>
2020-07-01 17:54:53 +0200

    including the whole structure of html- and js-Files

---
+++ public/src/v/manageInvoices.js
@@ -0,0 +1,23 @@
+car_rental.v.invoice.listAllInvoices = {
+    setupUserInterface: async function() {
+
+    },
+};
+
+car_rental.v.invoice.createInvoice = {
+    setupUserInterface: async function() {
+
+    },
+};
+
+car_rental.v.invoice.updateInvoice = {
+    setupUserInterface: async function() {
+
+    },
+};
+
+car_rental.v.invoice.deleteInvoice = {
+    setupUserInterface: async function() {
+
+    },
+};
---
+++ public/src/v/manageRentalAgreement.js
@@ -0,0 +1,23 @@
+car_rental.v.rentalAgreements.listAllRentAgreements = {
+    setupUserInterface: async function() {
+
+    },
+};
+
+car_rental.v.rentalAgreements.createRentAgreement = {
+    setupUserInterface: async function() {
+
+    },
+};
+
+car_rental.v.rentalAgreements.updateRentAgreement = {
+    setupUserInterface: async function() {
+
+    },
+};
+
+car_rental.v.rentalAgreements.deleteRentAgreement = {
+    setupUserInterface: async function() {
+
+    },
+};
